749cb6bf86342e968c83360ebb6e0d5dec57f5e7
ianmcaulay <mcaulay.ian@gmail.com>
2019-03-26 23:44:28 -0700

    Clean rename script

--- rename_synth.py
+++ rename_synth.py
@@ -1,24 +1,15 @@
 import os
 
 
-
-
 def add_synth(dir_):
     for filename in os.listdir(dir_):
-
         if filename.startswith('SYNTH_'):
             continue
 
         src = dir_ + '/' + filename
-        #dst ='xyz'+ dst
         dst = dir_ + '/' + 'SYNTH_' + filename
 
         os.rename(src, dst)
 
 
-
-
-
-[add_synth(f'data/VCTK-Corpus/synth_audio/p{n}') for n in [226, 227, 228, 229, 247]]
-
-
+[add_synth(f'data/VCTK-Corpus/synth_audio/p{n}') for n in [225, 226, 227, 228, 229, 247]]
